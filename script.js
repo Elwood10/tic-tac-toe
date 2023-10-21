95f0b1c45e59a70d5f0212e9099805a2b6b1989b
Roy Carter <roycartercpa@gmail.com>
2023-10-20 21:51:05 -0400

    set up initial files,started landing page with a chat GPT game image

---
+++ script.js
@@ -0,0 +1,5 @@
+let start = document.getElementById("startBtn")
+start.addEventListener("click", (e) => {
+    let start1 = document.getElementById("container1")
+    start1.setAttribute('style', 'display: none')
+})
